2797708d362a3c195d00b4f46b02dadd3de884c4
Henrry Valenzuela <henrryvalen@gmail.com>
2023-05-25 00:38:51 -0500

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,11 @@
 variable "resource_group" {
   description = "The resource group"
-  default     = "resourcegroupterra003"
+  default     = "resourcegroupterra004"
 }
 
 variable "application_name" {
   description = "The Spring Boot application name"
-  default     = "webappgroupterra003"
+  default     = "webappgroupterra004"
 }
 
 variable "location" {
