e818640afa41ea6fa352a9c856d61d422df3bbac
Henrry Valenzuela <henrryvalen@gmail.com>
2023-05-24 23:43:14 -0500

    Actualización

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,19 @@
+# We strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
 provider "azurerm" {
-  version = "=2.20.0"
   features {}
 }
+
 
 resource "azurerm_resource_group" "main" {
   name     = var.resource_group
@@ -13,23 +25,23 @@ resource "azurerm_resource_group" "main" {
 }
 
 resource "random_password" "password" {
-  length = 32
-  special = true
+  length           = 32
+  special          = true
   override_special = "_%@"
 }
 
 # This creates a MySQL server
 resource "azurerm_mysql_server" "main" {
-  name                              = "${azurerm_resource_group.main.name}-mysql-server"
-  location                          = azurerm_resource_group.main.location
-  resource_group_name               = azurerm_resource_group.main.name
+  name                = "${azurerm_resource_group.main.name}-mysql-server"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
 
-  administrator_login               = "petclinic"
-  administrator_login_password      = random_password.password.result
+  administrator_login          = "petclinic"
+  administrator_login_password = random_password.password.result
 
-  sku_name   = "B_Gen5_1"
-  storage_mb = 5120
-  version    = "5.7"
+  sku_name                          = "B_Gen5_1"
+  storage_mb                        = 5120
+  version                           = "5.7"
   auto_grow_enabled                 = true
   backup_retention_days             = 7
   geo_redundant_backup_enabled      = false
